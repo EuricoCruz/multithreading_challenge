86738463c74f200de7688bff3c17a235936bbfb6
@esouza <esouza@palo-it.com>
2025-08-01 19:51:54 -0300

    requisições finalizadas com sucesso

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+type CombinedResponse struct {
+	BrasilApi *BrasilApiResponse `json:"brasil_api"`
+	ViaCep    *ViaCepResponse    `json:"via_cep"`
+}
 
 type BrasilApiResponse struct {
 	Cep          string `json:"cep"`
@@ -49,11 +57,14 @@ func main() {
     })
 	r.Get("/cep/{cep}", handleCepSearch)
 
-    http.ListenAndServe(":8080", r)
+    log.Fatal(http.ListenAndServe(":8080", r))
+
 }
 
 func handleCepSearch (w http.ResponseWriter, r *http.Request) {
 	cep := chi.URLParam(r, "cep")
+	ctx, cancel := context.WithTimeout(r.Context(), 1*time.Second)
+	defer cancel()
 	fmt.Printf("Received CEP: %s\n", cep)
 
 	urlBrasilApi := "https://brasilapi.com.br/api/cep/v1/" + cep
@@ -69,14 +80,14 @@ func handleCepSearch (w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error making request to Brasil API", http.StatusInternalServerError)
 		return
 	}
+	defer respBrasilApi.Body.Close()
 
-	jsonData := &BrasilApiResponse{}
-	if err := json.NewDecoder(respBrasilApi.Body).Decode(jsonData); err != nil {
+	jsonDataBrasilApi := &BrasilApiResponse{}
+	if err := json.NewDecoder(respBrasilApi.Body).Decode(jsonDataBrasilApi); err != nil {
 		http.Error(w, "Error decoding response from Brasil API", http.StatusInternalServerError)
 		return
 	}
-	fmt.Printf("Decoded JSON data: %+v\n", jsonData)
-	defer respBrasilApi.Body.Close()
+	fmt.Printf("Decoded JSON data: %+v\n", jsonDataBrasilApi)
 	if respBrasilApi.StatusCode != http.StatusOK {
 		http.Error(w, "Error fetching data from Brasil API", respBrasilApi.StatusCode)
 		return
@@ -104,6 +115,13 @@ func handleCepSearch (w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Printf("Decoded ViaCep JSON data: %+v\n", jsonDataViaCep)
+
+	response := map[string]interface{}{
+		"brasil_api": jsonDataBrasilApi,
+		"via_cep":    jsonDataViaCep,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 
 
 }
